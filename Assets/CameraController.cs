5fd1adc146a821a5159684e4ba46662d228b94ea
Tobias Gagnon <90637945+TheShadowRaptor@users.noreply.github.com>
2023-04-07 13:41:17 -0300

    [ADDED] CameraPan

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> cameraPoints = new List<GameObject>();
+    [SerializeField] private int currentCameraPoint = 0;
+    [SerializeField] private float cameraPanSpeed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,38 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeCameraPosition();
+    }
+    
+    void ChangeCameraPosition()
+    {
+        // Change currentCameraPoint
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (currentCameraPoint == 0) currentCameraPoint = cameraPoints.Count - 1;
+            else currentCameraPoint -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentCameraPoint == cameraPoints.Count - 1) currentCameraPoint = 0;
+            else currentCameraPoint += 1;
+        }
+
+        // Camera lerps to currentCameraPoint untill it reaches it
+        Vector3 cameraPos = this.gameObject.transform.position;
+        Quaternion cameraRot = this.gameObject.transform.rotation;
+
+        if (cameraPos != cameraPoints[currentCameraPoint].transform.position)
+        {
+            cameraPos = Vector3.Lerp(cameraPos, cameraPoints[currentCameraPoint].transform.position, cameraPanSpeed);   
+            this.gameObject.transform.position = cameraPos;
+        }
+
+        if (cameraRot != cameraPoints[currentCameraPoint].transform.rotation)
+        {
+            cameraRot = Quaternion.Lerp(cameraRot, cameraPoints[currentCameraPoint].transform.rotation, cameraPanSpeed);   
+            this.gameObject.transform.rotation = cameraRot;
+        }
     }
 }
