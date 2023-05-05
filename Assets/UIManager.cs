35b1f8212e5eee83fce6e207bc83bd5d78502a64
DanielSnd <daniel.snd.anim@gmail.com>
2023-05-05 16:11:57 -0300

    UI manager

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,79 @@ using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
 
-public class UIManager : MonoBehaviour
-{
+public class UIManager : MonoBehaviour {
     
+    public UIDocument document;
+    private VisualElement root,mainMenuContainer,buttonsContainer;
+    private Label headerLabel;
+    public static bool IsInMenu = false;
+    void Start()
+    {
+        root = document.rootVisualElement;
+        mainMenuContainer = root.Query("Container");
+        buttonsContainer = root.Query("ButtonsContainer");
+        headerLabel = root.Query<Label>("HeaderLabel");
+        ShowMainMenu();
+    }
+
+    public void ShowMenu(string menuTitle) {
+        buttonsContainer.Clear();
+        headerLabel.text = menuTitle;
+        mainMenuContainer.SetDisplayBasedOnBool(true);
+        IsInMenu = true;
+    }
+
+    public void HideMenu() {
+        mainMenuContainer.SetDisplayBasedOnBool(false);
+        IsInMenu = false;
+    }
+
+    public void ShowMainMenu()
+    {
+        ShowMenu("PuzzleBlocks Yay");
+        buttonsContainer.Add(CreateButton("Play game", () =>
+        {
+            Debug.Log("Go to play game");
+        }));
+        buttonsContainer.Add(CreateButton("Settings", ShowSettingsMenu));
+        buttonsContainer.Add(CreateButton("Quit Game", () => {
+            Application.Quit();
+        }));
+    }
+
+    public void ShowSettingsMenu()
+    {
+        ShowMenu("Settings");
+        buttonsContainer.Add(CreateLabel("New label! This is a label! Isn't that cool?"));
+        buttonsContainer.Add(CreateSpacer());
+        buttonsContainer.Add(CreateLabel("Another label!"));
+        buttonsContainer.Add(CreateButton("A button!", ShowMainMenu));
+        buttonsContainer.Add(CreateButton("Another button!", ShowMainMenu));
+        buttonsContainer.Add(CreateSpacer(50));
+        buttonsContainer.Add(CreateButton("Back to main menu", ShowMainMenu));
+    }
+
+    public const string ButtonClass = "Button", MenuLabelClass = "MenuLabel";
+
+    public Button CreateButton(string buttonText, Action buttonAction)
+    {
+        Button newbutton = new Button(buttonAction);
+        newbutton.text = buttonText;
+        newbutton.AddToClassList(ButtonClass);
+        return newbutton;
+    }
+    
+    public Label CreateLabel(string buttonText)
+    {
+        Label newLabel = new Label(buttonText);
+        newLabel.AddToClassList(MenuLabelClass);
+        return newLabel;
+    }
+    
+    public VisualElement CreateSpacer(float space = 10)
+    {
+        VisualElement spacer = new VisualElement();
+        spacer.style.minHeight = space;
+        return spacer;
+    }
 }
